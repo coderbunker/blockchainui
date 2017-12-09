78958e3714a933c17ccd4fa99310ca89b074f9c7
Robert Ridley <robert.n.ridley@gmail.com>
2017-12-09 14:36:18 +0800

    add axios and add api call for data

--- src/App.js
+++ src/App.js
@@ -4,11 +4,25 @@ import Search from './components/Search';
 import CardsList from './components/CardsList';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://gist.githubusercontent.com/rngadam/2ad49e41cb7abfef7d672b4dcc0b26df/raw/00ec1e7a00b43b40cd7e569a387163ed4a7da6de/example.json')
+    .then((response) => { 
+      this.setState({data: response.data})
+      console.log(response.data)
+    });
+  }
   render() {
     return (
       <div className="App">
-        <Search />
-        <CardsList />
+        <Search data={this.state.data}/>
+        <CardsList data={this.state.data}/>
       </div>
     );
   }
--- src/components/CardsList.js
+++ src/components/CardsList.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import axios from 'axios';
+import { Row, Col } from 'react-bootstrap';
 
 export default class CardsList extends React.Component {
+
   render() {
+    const AllCards = this.props.data.map((single))
     return (
-      <div>CardsList</div>
+      <Row>
+        {AllCards}
+      </Row>
     )
   }
 }
