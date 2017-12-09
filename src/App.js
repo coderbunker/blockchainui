d30a3437b1c53362c6fac7265d86f2de64b39503
Robert Ridley <robert.n.ridley@gmail.com>
2017-12-09 21:29:47 +0800

    initial design for cards

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
-import Search from './components/Search';
+import SearchBar from './components/SearchBar';
 import CardsList from './components/CardsList';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: []
+      data: ''
     }
   }
 
   componentDidMount() {
     axios.get('https://gist.githubusercontent.com/rngadam/2ad49e41cb7abfef7d672b4dcc0b26df/raw/00ec1e7a00b43b40cd7e569a387163ed4a7da6de/example.json')
-    .then((response) => { 
+    .then((response) => {
       this.setState({data: response.data})
-      console.log(response.data)
+      console.log(this.state.data)
     });
   }
   render() {
     return (
       <div className="App">
-        <Search data={this.state.data}/>
+        <SearchBar data={this.state.data}/>
         <CardsList data={this.state.data}/>
       </div>
     );
--- src/components/CardsList.js
+++ src/components/CardsList.js
@@ -1,15 +1,86 @@
 import React from 'react';
-import { Row, Col, Media } from 'react-bootstrap';
+import { Row, Col, Media, Image, Label, Well } from 'react-bootstrap';
+import '../styles/cards.css';
 
 export default class CardsList extends React.Component {
 
   render() {
-    // const AllCards = this.props.data.map((SinglCard, index)
-    // )
+    const AllCards = data.map((SingleCard, index) => 
+      <Col md={4} sm={6} xs={12} key={index}>
+        <div className="rounded-card">
+          <Media>
+            <Media.Left>
+              <Image width={64} height={64} src={SingleCard.thumbnail_url} circle />
+            </Media.Left>
+            <Media.Body>
+              <Media.Heading>{SingleCard.full_name}</Media.Heading>
+              <div>
+                <span className="bold">{SingleCard.hourly_rate}</span><span> {SingleCard.hourly_rate_currency + "/hr"}</span>
+              </div>
+              <p>
+                {SingleCard.keywords.map((keyword, index) => 
+                <Label bsStyle="success" className="skill-label" key={index}>{keyword}</Label>
+              )}
+              </p>
+            </Media.Body>
+          </Media>
+        </div>
+      </Col>
+    )
+
     return (
-      <Row>
-        {/* {AllCards} */}
-      </Row>
+      <div className="container card-container">
+        <Row>
+          {this.props.data===[]? <div>Loading …</div>: AllCards}
+        </Row>
+      </div>
     )
   }
 }
+
+
+const data = []
+
+const dataObject = {
+  "full_name": "Ricky Ng-Adam",
+  "rates": [
+    {"type": "consulting", "hourly": "700", "currency": "RMB"}
+  ],
+  "hourly_rate" : "700",
+  "hourly_rate_currency": "RMB",
+  "location": "31.190751,121.426261,15",
+  "email": "rngadam@coderbunker.com",
+  "linkedin": "https://www.linkedin.com/in/rngadam/",
+  "mobile": ["+86-156-1894-3215"],
+  "github": "rngadam",
+  "organizations": { 
+    "Coderbunker": ["Founder", "Lead"]
+   },
+  "thumbnail_url": "https://tse1-mm.cn.bing.net/th?id=OIP.rnG1ojcmwebKASg3fRu2rwEsEs&w=100&h=105&c=8&rs=1&qlt=90&pid=3.1&rm=2",
+  "keywords": [ 
+    "development", 
+    "testing", 
+    "architecture", 
+    "design", 
+    "R&D", 
+    "engineering", 
+    "team", 
+    "recruitment",
+    "Javascript", 
+    "Python", 
+    "C/C++", 
+    "shell",
+    "REST", 
+    "Websockets", 
+    "GraphQL", 
+    "database", 
+    "PostgreSQL", 
+    "deployment", 
+    "Linux", 
+    "Ansible"
+  ]
+  }
+
+  for (var i = 0; i < 30; i++ ){
+    data.push(dataObject)
+  }
